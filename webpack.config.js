af8757bd32cbe100e4893b67b7231b37b1d20952
Nathan Venn <natevenn@gmail.com>
2016-07-31 15:20:34 -0600

    config webpack, add doctype html to index.html, create app component

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es']
+          presets: ['es2015', 'react']
         }
       }
     ]
