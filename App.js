8f8d380e103f8f9cad5c35f2adcdbf16fa58c58a
Nathan Venn <natevenn@gmail.com>
2016-08-01 10:41:56 -0600

    create a stateless component in App called widget

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super(); // super gives us context for this in our component
+    this.state = { txt: '' }
+    this.update = this.update.bind(this)
+  }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
   render() {
     return (
-      <div>Hi</div>
-    )
+      <div>
+        <Widget txt={this.state.txt} update={this.update} />
+      </div>
+      );
   }
 }
 
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
 export default App
-
-
