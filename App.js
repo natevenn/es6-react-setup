04dcd217be3f985db099c40ddaaea83822b739d5
Nathan Venn <natevenn@gmail.com>
2016-08-01 14:03:27 -0600

    add slider bar with each bar having own values

--- App.js
+++ App.js
@@ -3,33 +3,132 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
-    super(); // super gives us context for this in our component
-    this.state = { txt: '' }
+    super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     this.update = this.update.bind(this)
   }
 
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState ({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    });
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref='red' update={this.update} />
+        {'Red: ' + this.state.red}
+        <br/>
+        <Slider ref='green' update={this.update} />
+        {'Green: ' + this.state.green}
+        <br/>
+        <Slider ref='blue' update={this.update} />
+        {'Blue: ' + this.state.blue}
       </div>
-      );
+    )
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref='inp' type="range"
+          min='0'
+          max='255'
+          onChange={this.props.update} />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+//class App extends React.Component {
+  //constructor(){
+    //super(); // super gives us context for this in our component
+    //this.state = {
+      //red: 0,
+      //green: 0,
+      //blue: 0
+    //}
+    //this.update = this.update.bind(this)
+  //}
+
+  //update(e) {
+    //this.setState({
+      //red: ReactDOM.findDOMNode(this.refs.red).value,
+      //green: ReactDOM.findDOMNode(this.refs.green).value,
+      //blue: ReactDOM.findDOMNode(this.refs.blue).value
+    //})
+  //}
+
+  //render() {
+    //return (
+      //<div>
+        //<Slider ref='red' update={this.update} />
+        //{this.state.red}
+        //<br/>
+        //<Slider ref='green' update={this.update} />
+        //{this.state.green}
+        //<br/>
+        //<Slider ref='blue' update={this.update} />
+        //{this.state.blue}
+      //</div>
+      //);
+  //}
+//}
+
+//class Slider extends React.Component {
+  //render() {
+    //return (
+      //<input type="range"
+        //min='0'
+        //max='255'
+        //onChange={this.props.update} />
+    //);
+  //}
+//}
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+//export default App
